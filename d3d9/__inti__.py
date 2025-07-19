2b75cde568c8b8e31e7fdc80ee7b73962bcf806d
liaobin-org-ai-org <liaobin15863049682@outlook.com>
2025-07-19 15:41:55 +0800

    Resolve merge conflicts and add LICENSE

--- d3d9/__inti__.py
+++ d3d9/__inti__.py
@@ -1,3 +1,9 @@
+# MIT License
+#
+# Copyright (c) 2024 bin liao
+#
+# This software is licensed under the MIT License. See the LICENSE file in the project root for full license information.
+
 import pyglet
 from pyglet import gl, graphics
 from pyglet.window import Window
